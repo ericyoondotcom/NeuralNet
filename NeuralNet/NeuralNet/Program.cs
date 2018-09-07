f81e6e22816fdabf4160c7ac853fc0f22adfbc16
Eric <emjyoon@gmail.com>
2018-09-06 17:28:56 -0700

    Initial commit

---
+++ NeuralNet/NeuralNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NeuralNet
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
